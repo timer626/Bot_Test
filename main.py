1e56c13fa25ea40d9b401e9cfdbb6c759e313868
Первое сохраниние <timur.gumerov.00@mail.ru>
2023-04-29 15:40:10 +0300

    Первая часть статьи

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import misc
+import handlers
+import asyncio
+import logging
 
 
+async def main():
+    await misc.bot.delete_webhook(drop_pending_updates=True)
+    await misc.dp.start_polling(misc.bot)
 
 
-print("bye")
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
